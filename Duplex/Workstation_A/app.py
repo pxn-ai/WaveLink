5513f6a434d9e4b482ee618a93360b37d8df7f6f
pxn-ai <kaluarachchikapr.24@uom.lk>
2026-09-08 22:05:03 +0530

    Auto-sync:

--- Duplex/Workstation_A/app.py
+++ Duplex/Workstation_A/app.py
@@ -63,13 +63,7 @@ class WaveLinkApp(QMainWindow):
             dummy = " ".ljust(1024, ' ')
             self.tx_socket.sendto(dummy.encode('utf-8'), ("127.0.0.1", 5001))
             
-            bubble = f"""
-            <div align='right'>
-                <span style='background-color: #DCF8C6; color: black; font-size: 16px; padding: 5px 10px;'>
-                    {text}
-                </span>
-            </div><br>
-            """
+            bubble = f"<table width='100%'><tr><td align='right'><span style='background-color:#DCF8C6; color:black; font-size:16px;'>&nbsp;&nbsp;{text}&nbsp;&nbsp;</span></td></tr></table>"
             self.chat_history.append(bubble)
             self.input_field.clear()
             
@@ -77,16 +71,9 @@ class WaveLinkApp(QMainWindow):
             scrollbar = self.chat_history.verticalScrollBar()
             scrollbar.setValue(scrollbar.maximum())
 
-    def display_message(self, text):
-        bubble = f"""
-        <div align='left'>
-            <span style='background-color: #FFFFFF; color: black; font-size: 16px; padding: 5px 10px;'>
-                {text}
-            </span>
-        </div><br>
-        """
-        self.chat_history.append(bubble)
-        
+            bubble = f"<table width='100%'><tr><td align='left'><span style='background-color:#FFFFFF; color:black; font-size:16px;'>&nbsp;&nbsp;{text}&nbsp;&nbsp;</span></td></tr></table>"
+            self.chat_history.append(bubble)
+            
         # Force auto-scroll to bottom
         scrollbar = self.chat_history.verticalScrollBar()
         scrollbar.setValue(scrollbar.maximum())
